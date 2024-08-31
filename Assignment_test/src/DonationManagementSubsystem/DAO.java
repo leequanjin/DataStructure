835b67a42d7b99510fafd91d82fea243f546a948
chongaaaa <hengpeilin@gmail.com>
2024-08-31 18:55:34 +0800

    mau pergi makan la

---
+++ Assignment_test/src/DonationManagementSubsystem/DAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DonationManagementSubsystem;
+
+/**
+ *
+ * @author Asus
+ */
+public class DAO {
+    
+}
